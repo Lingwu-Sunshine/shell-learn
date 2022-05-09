24c1f0d967b8da979bbad2b33dcb94ad5bf52838
灵悟 <3027335568@qq.com>
2022-05-09 20:07:12 +0800

    第十九节例子

---
+++ demo19/add_demo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+############进行:+扩展变量的练习###############
+var="var1"
+
+echo "此时变量的值为:$var,${var:+"word"}"
+
+unset var
+
+echo "取消变量，此时变量的值为:$var,${var:+"word"}"
+
+echo "此时变量的值为$var"
