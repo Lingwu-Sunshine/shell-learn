c9d3377b1dd34be7696f128d0d36da4b0e30785f
灵悟 <3027335568@qq.com>
2022-05-16 14:04:01 +0800

    第四十八节例子

---
+++ demo48/alias_demo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###第四十八节alias例子练习###
+
+echo "输出所有的alias别名`alias`"
+
+#echo "起一个别名"
+#alias lsall ls -l
+
+#echo "取消别名"
+#unalias lsall
---
+++ demo48/function_demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+###第四十八节函数功能例子练习###
+
+function printf_example(){
+echo "我是一个函数"
+
+}
+
+printf_example
